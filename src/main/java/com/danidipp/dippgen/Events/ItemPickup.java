c383b8c74f95b5eb995cc6a03e3dbb333633741b
DaniDipp <dippigames@gmail.com>
2024-02-16 13:52:13 +0000

    require sneakying for item pickup

---
+++ src/main/java/com/danidipp/dippgen/Events/ItemPickup.java
@@ -0,0 +1,20 @@
+package com.danidipp.dippgen.Events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
+
+import com.danidipp.dippgen.Plugin;
+
+public class ItemPickup implements Listener {
+	@EventHandler
+	public void onItemPickup(PlayerAttemptPickupItemEvent event) {
+		Plugin.plugin.getLogger().info("Item pickup event");
+		if (!event.getPlayer().isSneaking()){
+			event.setCancelled(true);
+			var item = event.getItem();
+			var pickupDelay = Math.max(5, item.getPickupDelay());
+			item.setPickupDelay(pickupDelay);
+		}
+	}
+}
